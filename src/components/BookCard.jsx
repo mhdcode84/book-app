4cb6be6ecf94db2a5b4fd85c85504629bf9a4f4d
mohamad sajadi <mohamadcodew84@gmail.com>
2025-04-22 08:15:09 +0330

    render bookcard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,10 +1,21 @@
-
-function BookCard({data:{image}}) {
+import { CiHeart } from "react-icons/ci";
+function BookCard({data:{image,title,author,language,pages}}) {
     console.log(image);
     
   return (
-    <div>
-      <img src={image} alt="" />
+    <div className="flex justify-between">
+      <img src={image} alt={title} />
+      <div>
+        <h3>{title}</h3>
+        <p>{author}</p>
+      </div>
+      <div>
+        <span>{language}</span>
+        <span>{pages}pages</span>
+      </div>
+      <button >
+    <CiHeart color="#e0e0e0" fontSize="1.8rem"/>
+      </button>
     </div>
   )
 }
