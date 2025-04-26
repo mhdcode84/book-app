d2cb01093d382975702aa6e151d1273e220835a5
mohamad sajadi <mohamadcodew84@gmail.com>
2025-04-26 15:06:42 +0330

    complete project styled & SearchBox

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,32 +1,43 @@
 import React, { useState } from 'react'
-import { books } from '../constants/mockData'
+import { books as bookData } from '../constants/mockData'
 import BookCard from './BookCard'
 import SideCard from './SideCard'
+import SearchBox from './SearchBox'
 function Book() {
-  const [liked,setliked] = useState([])
-  function handlelikedlist(book,status){
-if (status) {
-  const newlikelist = liked.filter(i=>i.id!==book.id)
-  setliked(newlikelist)
+  const [liked, setliked] = useState([])
+  const [books,setbooks] = useState(bookData)
+  const [search, setsearch] = useState([])
+  function handlelikedlist(book, status) {
+    if (status) {
+      const newlikelist = liked.filter(i => i.id !== book.id)
+      setliked(newlikelist)
+    } else {
+      setliked((liked) => [...liked, book])
+
+
+    }
+  }
+  function searchhandler(){
+if(search){
+const newbooks = bookData.filter((book)=>book.title.toLocaleLowerCase().includes(search))
+setbooks(newbooks)
 }else{
-setliked((liked)=>[...liked,book])
-
-
+  setbooks(bookData)
 }
-
-
-
   }
   return (
-    <div className={liked.length ? "flex": ""}>
-    <div className='flex-1'>{books.map(book=>(
-      <BookCard data={book} handlelikedlist={handlelikedlist} key={book.id} />
+    <>
+      <SearchBox search={search} setsearch={setsearch} searchhandler={searchhandler} />
+      <div className={liked.length ? "flex" : ""}>
+        <div className='flex-1'>{books.map(book => (
+          <BookCard data={book} handlelikedlist={handlelikedlist} key={book.id} />
 
-    ))}</div>
-    {!!liked.length && <div className='bg-[#6552f5] p-[15px] rounded-[10px] ml-[20px] h-fit'> 
-      <h4 className='text-[20px] font-medium'>Favorite</h4>
-    {liked.map(book=><SideCard data={book} key={book.id} />)}</div>}
-    </div>
+        ))}</div>
+        {!!liked.length && <div className='bg-[#6552f5] p-[15px] rounded-[10px] ml-[20px] h-fit'>
+          <h4 className='text-[20px] font-medium'>Favorite</h4>
+          {liked.map(book => <SideCard data={book} key={book.id} />)}</div>}
+      </div>
+    </>
   )
 }
 
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BiVector } from 'react-icons/bi';
+import { MdSearch } from "react-icons/md";
 
-function SearchBox() {
+function SearchBox({search,setsearch,searchhandler}) {
   return (
-    <div>SearchBox</div>
+    <div className=' w-fit mb-[50px] flex gap-3'>
+        <input className='border w-[300px] h-[40px] rounded-[5px] pl-[10px] pb-1 outline-none border-none bg-[#e0e0e0] text-[#090909] text-[1rem] flex items-center' placeholder='search title' type="text" value={search} onChange={e=>setsearch(e.target.value.toLocaleLowerCase().trim())} />
+        <button className='bg-[#6552f4] w-[40px] h-[40px] flex justify-center items-center cursor-pointer rounded-[5px]' onClick={searchhandler}>
+            <MdSearch className='text-2xl'  />
+        </button>
+    </div>
   )
 }
-
 export default SearchBox
